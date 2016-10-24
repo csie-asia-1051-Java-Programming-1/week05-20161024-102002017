c080c5c369335edd50188c85d9023b07faca1b6c
102002017 <102002017@live.asia.edu.tw>
2016-10-24 16:02:58 +0800

    課堂練習

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,13 +2,27 @@
 /*
  * Topic: 讓使用者輸入一正整數n，用while迴圈計算n!後輸出。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 102002017 邱泓偉
  */
 
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int i = 1;
+		int a = 1;
+		
+		while(i <= n)
+		{
+			a = a*i;
+			i++;
+		}
+		
+		System.out.println(a);
 
 	}
 
--- src/ex/ex05.java
+++ src/ex/ex05.java
@@ -2,7 +2,7 @@
 /*
  * Topic: 設計一個程式由 0~9 的亂數隨機產生一 10*10 的二維陣列，將行列互換輸出(即輸出轉置矩陣)
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 102002017 邱泓偉
  */
 
 public class ex05 {
@@ -10,6 +10,48 @@ public class ex05 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int [][] n = new int[10][10];
+		
+		for(int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				n[i][j] = (int)(Math.random()*10);
+			}
+		}
+		
+		for(int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				System.out.print("n[" +  i +"][" + j + "] = " + n[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		
+		//===================================
+		
+		int[][] m = new int[10][10];
+		
+		for(int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				m[i][j] = n[j][i];
+			}
+		}
+		
+		//=======================================
+		System.out.println("轉置矩陣");
+		
+		for(int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				System.out.print("n[" +  i +"][" + j + "] = " + m[i][j] + "\t");
+			}
+			System.out.println();
+		}
 	}
 
 }
